4ba9341c282a863bc52b4c2432f76b6102d25de1
Kurt Garcia <akod3vs@gmail.com>
2018-10-15 08:02:21 -0700

    one page

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { ItemsComponent } from './components/items/items.component';
+
+import { ItemService } from './services/item.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
+  ],
+  providers: [ItemService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
